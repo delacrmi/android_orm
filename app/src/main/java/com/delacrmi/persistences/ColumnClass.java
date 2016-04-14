b2ff1b16cebb130dc88e2c6725012c518af95d67
delacrmi <delacrmi@gmail.com>
2016-04-14 01:10:16 -0400

    changed the access to ColumnClass class

--- app/src/main/java/com/delacrmi/persistences/ColumnClass.java
+++ app/src/main/java/com/delacrmi/persistences/ColumnClass.java
@@ -3,7 +3,7 @@ package com.delacrmi.persistences;
 /**
  * Created by delacrmi on 14/4/2016.
  */
-public class ColumnClass {
+class ColumnClass {
     public String name;
     public int type;
     public String value;
