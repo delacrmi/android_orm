53e097a9c9793da474de7b00a46e020e5ffe9249
Miguel <delacrmi@users.noreply.github.com>
2016-04-28 13:28:57 -0400

    Update ColumnClass.java

--- app/src/main/java/com/delacrmi/persistences/ColumnClass.java
+++ app/src/main/java/com/delacrmi/persistences/ColumnClass.java
@@ -7,4 +7,23 @@ class ColumnClass {
     public String name;
     public int type;
     public String value;
+    
+    private int index;
+    private int length;
+    private String relationshipType;
+    private String [] relationshipColumns;
+    
+    /*returns true if the column is 
+    of relationship type of the 
+    otherwise returns false*/
+    public boolean isRelationshipColumn(){
+        return relationshipType != null ? true : false; 
+    }
+    
+    /*return the String array structure*/
+    @Nullable
+    public String[] getRelationshipColumns(){
+        return relationshipColumns;
+    }
+    
 }
